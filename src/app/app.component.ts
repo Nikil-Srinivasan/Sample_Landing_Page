24e942c18c01b53b0d9578f0c670d46de5ef8a71
Nikil Srinivasan <nikil.srinivasan@kovai.co>
2024-09-08 19:31:14 +0530

    generate landing page component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { LandingPageComponent } from './landing-page/landing-page.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [],
+  imports: [LandingPageComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styles: []
 })
 export class AppComponent {
   title = 'css_project';
