a93d28ef87325e49c6a7a8230e3772a3aad5625e
Nikil Srinivasan <nikil.srinivasan@kovai.co>
2024-09-08 22:02:57 +0530

    develop hero section

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -1,14 +1,15 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { HeroSectionComponent } from './sections/hero-section/hero-section.component';
 
 @Component({
   selector: 'app-landing-page',
   standalone: true,
   imports: [
     CommonModule,
+    HeroSectionComponent,
   ],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.css',
-  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class LandingPageComponent { }
