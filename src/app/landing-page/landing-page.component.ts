41274f97cea4c798d10e3df3ad895e9bf329b834
Nikil Srinivasan <nikil.srinivasan@kovai.co>
2024-09-08 22:06:11 +0530

    set hero section bg color

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { HeroSectionComponent } from './sections/hero-section/hero-section.component';
+import { ClientSectionComponent } from './sections/client-section/client-section.component';
 
 @Component({
   selector: 'app-landing-page',
@@ -8,6 +9,7 @@ import { HeroSectionComponent } from './sections/hero-section/hero-section.compo
   imports: [
     CommonModule,
     HeroSectionComponent,
+    ClientSectionComponent
   ],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.css',
