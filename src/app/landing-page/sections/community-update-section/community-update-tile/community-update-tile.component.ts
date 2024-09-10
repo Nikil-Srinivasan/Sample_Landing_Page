032b11f29b9bf9bfac9e6ce2dd7e04e29df49ebf
Nikil Srinivasan <nikil.srinivasan@kovai.co>
2024-09-10 20:19:11 +0530

    format document

--- src/app/landing-page/sections/community-update-section/community-update-tile/community-update-tile.component.ts
+++ src/app/landing-page/sections/community-update-section/community-update-tile/community-update-tile.component.ts
@@ -2,17 +2,13 @@ import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
 import { CommunityDetails } from '../../../models/landing-page.models';
 
-
 @Component({
   selector: 'app-community-update-tile',
   standalone: true,
-  imports: [
-    CommonModule,
-  ],
+  imports: [CommonModule],
   templateUrl: './community-update-tile.component.html',
   styleUrl: './community-update-tile.component.css',
 })
 export class CommunityUpdateTileComponent {
   @Input() communityDetails!: CommunityDetails;
-
- }
+}
